4a08fa74955b5e26ad907251a9dc18dfdc00b0ad
ibrahim Soltan <ibrahimsoltan2000@gmail.com>
2023-05-01 15:57:38 +0300

    minor change

--- src/main/java/com/example/sellingservice/Entities/customerAndOrderId.java
+++ src/main/java/com/example/sellingservice/Entities/customerAndOrderId.java
@@ -1,51 +1,52 @@
-package com.example.sellingservice.Entities;
-
-import java.io.Serializable;
-
-public class customerAndOrderId implements Serializable {
-    int id;
-    String email;
-    String address;
-    String password;
-    int orderId;
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+//package com.example.sellingservice.Entities;
+//
+//import java.io.Serializable;
+//
+//public class customerAndOrderId implements Serializable {
+//    int id;
+//    String email;
+//    String address;
+//    CustomerOrder customerOrder;
+//
+//
+//
+//    public CustomerOrder getCustomerOrder() {
+//        return customerOrder;
+//    }
+//
+//    public void setCustomerOrder(CustomerOrder customerOrder) {
+//        this.customerOrder = customerOrder;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
